ff734c399231f94bc10f5bb1bde1980cd67696bc
Joe Greener <jgreener@hotmail.co.uk>
2024-07-30 14:15:27 +0100

    progres search

--- progres_search/views.py
+++ progres_search/views.py
@@ -3,11 +3,17 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.utils import timezone
 import progres as pg
+import torch
+from torch_geometric.loader import DataLoader
+import os
 
 from .models import Submission
 from .forms import UploadFileForm
 
 pg_model = pg.load_trained_model()
+targetdb = "scope40"
+target_fp = os.path.join(pg.database_dir, targetdb + ".pt")
+target_data = torch.load(target_fp)
 
 def read_pdb_coords(f):
     s = f.read().decode("utf-8")
@@ -39,4 +45,22 @@ def index(request):
 
 def results(request, submission_id):
     submission = get_object_or_404(Submission, pk=submission_id)
-    return render(request, "progres_search/results.html", {"submission": submission})
+    data_loader = DataLoader(
+        pg.EmbeddingDataset(torch.tensor(submission.embedding)),
+        batch_size=1,
+        shuffle=False,
+        num_workers=0,
+    )
+    result_dict = list(pg.search_generator_inner(
+        data_loader, ["?"], targetdb, target_data, None, "torch",
+    ))[0]
+    result_zip = zip(result_dict["domains"], result_dict["hits_nres"],
+                     result_dict["similarities"], result_dict["notes"])
+    context = {
+        "submission"   : submission,
+        "targetdb"     : targetdb,
+        "minsimilarity": result_dict["minsimilarity"],
+        "maxhits"      : result_dict["maxhits"],
+        "results"      : result_zip,
+    }
+    return render(request, "progres_search/results.html", context)
